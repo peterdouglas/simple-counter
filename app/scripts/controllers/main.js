550689af72735b4c518756e5b921dce20f213bd4
Peter Ince <pdince@gmail.com>
2015-09-27 00:55:35 +1000

    feat(form): added service, controller and form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,39 @@
  * Controller of the frontendTestApp
  */
 angular.module('testApp.controllers', [])
-  .controller('MainCtrl', ['CounterService', function (counterService) {
+  .controller('MainCtrl', ['CounterService', '$scope', function (counterService, $scope) {
+    $scope.records = [];
+    $scope.totalCount = 0;
+    $scope.user = {};
 
-    this.addCounter = function() {
-        counterService.addCounter();
+    // Create a function to set the records as it is a repetitive process
+    function processResponse(response) {
+      if(response.status === 200 && response.data) {
+        var tempNum = 0;
+        $scope.user = {};
+        response.data.map(function(value) {
+          tempNum += parseInt(value.count);
+        });
+        $scope.totalCount = tempNum;
+        $scope.records = response.data;
+      }
+    }
+    counterService.getAllCounters().then(processResponse);
+
+    $scope.addCounter = function(name) {
+      counterService.addCounter(name).then(processResponse);
       };
+
+    $scope.incCounter = function(userId) {
+      counterService.incCounter(userId).then(processResponse);
+    };
+
+    $scope.decCounter = function(userId) {
+      counterService.decCounter(userId).then(processResponse);
+    };
+
+    $scope.delCounter = function(userId) {
+      counterService.delCounter(userId).then(processResponse);
+    };
 
   }]);
--- app/scripts/services/counterService.js
+++ app/scripts/services/counterService.js
@@ -8,8 +8,42 @@
  * Counter service  of the frontendTestApp
  */
 angular.module('testApp.services', [])
-  .factory('CounterService', function () {
-  return {
+  .factory('CounterService', ['$http', function ($http) {
+    function successResponse(response) {
+      return {
+        status: response.status,
+        data: response.data
+      };
+    }
+
+    function errorResponse() {
+      throw errorResponse.status + " : " + errorResponse.data;
+
+    }
+
+    return {
+
+      addCounter: function(name) {
+        return $http.post('http://localhost:3000/api/v1/counter/', {title:name}).
+          then(successResponse, errorResponse);
+      },
+      getAllCounters: function() {
+        return $http.get('http://localhost:3000/api/v1/counters/').
+          then(successResponse, errorResponse);
+      },
+      incCounter: function(userId) {
+        return $http.post('http://localhost:3000/api/v1/counter/inc', {id:userId}).
+          then(successResponse, errorResponse);
+      },
+      decCounter: function(userId) {
+        return $http.post('http://localhost:3000/api/v1/counter/dec', {id: userId}).
+          then(successResponse, errorResponse);
+      },
+      delCounter: function(userId) {
+        console.log(userId);
+        return $http.delete('http://localhost:3000/api/v1/counter/?id=' + userId).
+          then(successResponse, errorResponse);
+      }
 
   };
-  });
+  }]);
